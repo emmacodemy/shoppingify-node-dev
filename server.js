afd2e4ea024fcb05c995be8cea4dc55005213cfd
emmacodemy <eayodeleof18@gmail.com>
2021-01-15 15:48:52 +0100

    deployment and configuration

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ mongoose
   })
   .catch((e) => console.log(e));
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 const server = app.listen(port, () => {
   console.log(`App is running on port ${port}...`);
